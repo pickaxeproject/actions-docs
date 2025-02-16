0d850f7f2f6e669783344f96d396021967a6dd46
nathanielmhld <nathanielmhld@berkeley.edu>
2025-02-15 19:39:42 -0800

    adjustments

--- pickaxe-actions-docs/docusaurus.config.ts
+++ pickaxe-actions-docs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://pickaxeproject.github.io',
+  url: 'https://docs.pickaxe.co', 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/actions-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
